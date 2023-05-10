774866424534cd3157a71b4f6f7affc4a2a73236
marcioferraz81 <82520868+marcioferraz81@users.noreply.github.com>
2023-05-10 09:06:02 -0300

    Update Categoria.cs

--- AulaBD_FatecItu/Class/Categoria.cs
+++ AulaBD_FatecItu/Class/Categoria.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -84,6 +84,26 @@ namespace AulaBD_FatecItu.Class
                 cmd.ExecuteNonQuery();
 
                 return "excluído";
+            }
+            catch (Exception e)
+            {
+                return "erro: " + e.Message;
+            }
+        }
+
+        public static string editar(Categoria c)
+        {
+            try
+            {
+                MySqlConnection conn = Conexao.obterConexao();
+                string sql = "UPDATE categoria SET nome = @nome WHERE id = @id";
+                //string sql = "UPDATE categoria SET nome = @nome, email = @email WHERE id = @id";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", c.Id);
+                cmd.Parameters.AddWithValue("@nome", c.Nome);
+                //cmd.Parameters.AddWithValue("@email", c.Email);
+                cmd.ExecuteNonQuery();
+                return "editado com sucesso";
             }
             catch (Exception e)
             {
